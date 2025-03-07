dc571c8e28f8e1069f2eaa736b09ad3e89e3d2d7
p3dm <manh.pd225361@sis.hust.edu.vn>
2025-03-08 04:41:54 +0700

    add daisyUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Grid from "@/components/Grid";
 import  Hero  from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems } from "@/data";
 
 
 export default function Home() {
@@ -8,6 +10,7 @@ export default function Home() {
     <main className="relative bg-black-100 flex justify-center items-center
      flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
